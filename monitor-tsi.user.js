ef0a6c08387e3d33b16093e9d604c67570948108
brendosep164 <brendoale@gmail.com>
2026-05-16 01:33:32 -0300

    Update monitor-tsi.user.js

--- monitor-tsi.user.js
+++ monitor-tsi.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Monitor Operacional TSI
 // @namespace    http://tampermonkey.net/
-// @version      13.4
+// @version      14.0
 // @description  Monitor de apontamentos em tempo real com escalados vs apontados
 // @author       TSI
 // @match        https://tsi-app.com/planejamento-operacional*
